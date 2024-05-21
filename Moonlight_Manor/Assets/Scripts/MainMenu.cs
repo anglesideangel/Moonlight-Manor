11e5c04b438b3e120605aca61f7b62a963eda0d6
Chris Schlenker <schlenkerchristopher@gmail.com>
2024-05-21 15:37:17 -0700

    Menu Multiplayer Integration
    Local Host/Join integrated in menu

--- Moonlight_Manor/Assets/Scripts/MainMenu.cs
+++ Moonlight_Manor/Assets/Scripts/MainMenu.cs
@@ -2,13 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.Netcode;
+
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void PlayGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    public void HostGame()
+    {
+        NetworkManager.Singleton.StartHost();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    public void QuitGame(){
+
+    public void JoinGame()
+    {
+        NetworkManager.Singleton.StartClient();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
         Application.Quit();
     }
 }
