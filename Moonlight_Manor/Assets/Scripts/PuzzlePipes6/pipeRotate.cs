469a6353cc5ea75b33dfa4db3145a4ae1d3f9d2b
mchoi084 <114121808+mchoi084@users.noreply.github.com>
2024-06-04 13:55:10 -0700

    updated pipes

--- Moonlight_Manor/Assets/Scripts/PuzzlePipes6/pipeRotate.cs
+++ Moonlight_Manor/Assets/Scripts/PuzzlePipes6/pipeRotate.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+public enum PipeType
+{
+    Line,
+    Curve,
+    Cross
+}
+
 public class pipeRotate : MonoBehaviour
 {
     public static event Action<string, int> Rotated = delegate { };
-
+    public PipeType pipeType; // The type of the pipe
     private int numberRotated;
     void Start()
     {
@@ -27,10 +34,24 @@ public class pipeRotate : MonoBehaviour
             yield return new WaitForSeconds(0.01f);
         }
 
-        numberRotated += 1;
-
-        if (numberRotated > 3){
+        if (pipeType == PipeType.Line)
+        {
+            // For line pipes, rotate through 0, 1, 0, ...
+            numberRotated = (numberRotated + 1) % 2;
+        }
+        else if (pipeType == PipeType.Cross)
+        {
+            // For cross pipes, always stay at 0 regardless of rotations
             numberRotated = 0;
+        }
+        else if (pipeType == PipeType.Curve)
+        {
+            numberRotated += 1;
+
+            if (numberRotated > 3)
+            {
+                numberRotated = 0;
+            }
         }
         Rotated(name,numberRotated);
     }
