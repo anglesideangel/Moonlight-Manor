29f80282107dcec90d615c67b10c7f9aeb950245
anglesideangel <102494188+anglesideangel@users.noreply.github.com>
2024-06-04 01:10:56 -0700

    .

--- Moonlight_Manor/Assets/Scripts/PickUp.cs
+++ Moonlight_Manor/Assets/Scripts/PickUp.cs
@@ -28,12 +28,12 @@ public class PickUp : MonoBehaviour
     }
     private void OnMouseDrag(){
         // Get the current mouse position in screen space
-        Vector3 currentMousePosition = Input.mousePosition;
+        Vector3 currentMousePosition = Input.mousePosition - mousePosition;
         // Convert the screen space position to world space
         Vector3 worldPosition = Camera.main.ScreenToWorldPoint(currentMousePosition);
         
         // Only take x and z coordinates, keep y the same as the object's current position
-        transform.position = new Vector3(worldPosition.x, transform.position.y, worldPosition.z);
+        transform.position = new Vector3(worldPosition.z, transform.position.y, worldPosition.x);
     }
 
     private void OnMouseUp(){
